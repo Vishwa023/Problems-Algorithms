fef2aa8dc6cd37f6eca3a4f5ee47d50d3145eb0a
vishwa23 <vishwa.patel@treebohotels.com>
2020-05-02 19:33:25 +0530

    (nCr) file

---
+++ Problems/find_(nCr).cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+int main()
+{
+	ll n,k;
+	cin >> n >> k;
+
+	ll dp[n+5][k+5];
+	for(ll i=0;i<=n;i++)
+	{
+		for(ll j=0;j<=min(i,k);j++)
+		{
+			if(i==0 || j==i)
+			{
+				dp[i][j]=1;
+			}
+			else
+			{
+				dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+			}
+		}
+	}
+
+	cout << dp[n][k];
+
+	return 0;
+}
+
+// formula used
+
+// (n,r)=(n-1,r-1)+(n-1,r);
+
+// virender.singh@proptiger.com/aardetechnosoft --> ReactJS
+
+// webdev@123   -->>> JavaScript
+// jaydeeperaniya17jd@gmail.com
