e4596d24c278c8fab9345d7970593b1ea26d2fc0
vishwa23 <vishwa.patel@treebohotels.com>
2020-05-27 18:53:08 +0530

    adding basic lower and upper bound code using binary Search

---
+++ Problems/lower_upper.cpp
@@ -0,0 +1,47 @@
+// lower_bound code
+
+int lowerBound(int *a,int n,int key){
+    int s =0,e=n-1;
+    int ans = -1;
+
+    while(s<=e){
+        int mid = (s+e)/2;
+
+        if(a[mid]==key){
+            ans = mid;
+            e = mid - 1;
+        }
+        else if(a[mid]>key){
+            e = mid - 1;
+        }
+        else{
+            s = mid + 1;
+        }
+    }
+
+    return ans;
+}
+
+// upper_bound code
+
+int upperBound(int *a,int n,int key){
+    int s =0,e=n-1;
+    int ans = -1;
+
+    while(s<=e){
+        int mid = (s+e)/2;
+
+        if(a[mid]==key){
+            ans = mid;
+            s = mid+1;
+        }
+        else if(a[mid]>key){
+            e = mid - 1;
+        }
+        else{
+            s = mid + 1;
+        }
+    }
+
+    return ans;
+}
