58b3f10307f04bde721b3b96be7ec1a3bc712aae
vishwa23 <vishwa.patel@treebohotels.com>
2020-06-12 18:24:11 +0530

    changing sieve file

--- Algorithms/sieve.cpp
+++ Algorithms/sieve.cpp
@@ -2,13 +2,13 @@ const ll N = 1e5+5;
 ll prime[N];
 void sieve()
 {
-	memset(prime,1,sizeof(prime));
+	memset(prime,0,sizeof(prime));
 	for(ll i = 2; i*i <= N; i++)
 	{
-		if(prime[i])
+		if(!prime[i])
 		{
 			for(ll j=i*i;j<=N;j+=i)
-				prime[i] = 0;
+				prime[j] = 1;
 		}
 	}
 }
